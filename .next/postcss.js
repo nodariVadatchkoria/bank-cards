5c344cc71df3bfe8eca27dbc8fb01dd6d017b2d0
Nodari Vadatchkoria <n.vadatchkoria@alte.edu.ge>
2025-08-31 18:17:06 +0400

    Add build output for PostCSS integration
    Generated new build artifacts for PostCSS support in the Next.js project, including chunk files and runtime initialization. This enables PostCSS processing with Tailwind CSS plugin and tracks dependencies and environment variables for development.

---
+++ .next/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_5400308b._.js")
+R.c("build/chunks/[root-of-the-server]__40d7ef63._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/bank-card/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/GitHub/bank-card/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
